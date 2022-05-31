cf68244ce053a2db2b6efe46db1849778965e3c8
sanjay <ksanjayk@protonmail.com>
2022-05-31 17:53:49 +0530

    initial commit

---
+++ utils/errors/custom-error.js
@@ -0,0 +1,16 @@
+class CustomAPIError extends Error {
+
+    constructor(message,statusCode){
+        super(message)
+        this.statusCode = statusCode
+    }
+}
+
+const createCustomError =(msg,statusCode)=>{
+    CustomAPIError(msg,statusCode)
+}
+
+module.exports ={
+    CustomAPIError,
+    createCustomError
+}
---
+++ utils/middleware/not-found.js
@@ -0,0 +1,4 @@
+const notFound = (req, res) =>
+  res.status(404).json({ msg: "Route doesn't exit" });
+
+module.exports = notFound;
