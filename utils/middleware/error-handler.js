91ae65d27b0236f1e7889755f04f5e0e8c2db7df
sanjay <ksanjayk@protonmail.com>
2022-06-05 16:23:39 +0530

    used StatusCodes library

--- utils/middleware/error-handler.js
+++ utils/middleware/error-handler.js
@@ -1,11 +1,14 @@
-const {CustomAPIError} = require('../errors/custom-error')
+const { StatusCodes } = require("http-status-codes");
+const { CustomAPIError } = require("../errors/custom-error");
 
 const errorHandlerMiddleware = (err, req, res, next) => {
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message });
   }
 
-  return res.status(500).json({ msg: "Something Went Wrong" });
+  return res
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    .json({ msg: "Something Went Wrong" });
 };
 
 module.exports = errorHandlerMiddleware;
--- utils/middleware/not-found.js
+++ utils/middleware/not-found.js
@@ -1,4 +1,6 @@
+const { StatusCodes } = require("http-status-codes");
+
 const notFound = (req, res) =>
-  res.status(404).json({ msg: "Route doesn't exit" });
+  res.status(StatusCodes.NOT_FOUND).json({ msg: "Route doesn't exit" });
 
 module.exports = notFound;
